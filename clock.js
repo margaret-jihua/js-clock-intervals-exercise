52b33be8e1156412e3ec0920b36002b33182e14f
margaret-jihua <margaret.jihua@gmail.com>
2020-07-17 12:25:34 -0700

    Clock is walking...

--- clock.js
+++ clock.js
@@ -1,3 +1,7 @@
+let seconds = 0
+let minutes = 0
+let hours = 0
+
 function secondRotation(second){
     let degree = second * 6;
     return degree;
@@ -9,12 +13,47 @@ function minuteRotation(minutes, seconds){
     return degree;
 }
 
-console.log(minuteRotation(10,20));
-
 function hourRotation(hours, minutes){
     let hrToMin = hours * 60 + minutes;
     let degree = hrToMin * .5;
     return degree;
 }
 
-console.log(hourRotation(3,3));
+function tick(){
+    seconds ++;
+
+    if (seconds >= 60){
+        senconds = 0;
+        minutes ++;
+    }
+    
+    if (minutes >= 60){
+        minutes = 0;
+        hours ++;
+    }
+
+    if (hours >= 12){
+        hours = 0;
+    }
+    
+    // sting interpolation
+    // console.log('${hours}:${minutes}:${senconds}');
+    
+    secondHand.style.transform = 'rotate(' + secondRotation(seconds) + 'deg)';
+    minuteHand.style.transform = 'rotate(' + minuteRotation(minutes, seconds) + 'deg)';
+    hourHand.style.transform = 'rotate(' + hourRotation(hours, minutes) + 'deg)';
+
+}
+
+
+//// this is code gets run
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    secondHand = document.getElementById("second");
+    minuteHand = document.querySelector('#minute');
+    hourHand = document.querySelector('#hour');
+
+    setInterval(tick, 1000);
+})
+
